2cd230836c9ff6b09bdaa2ad1775bb8c1ab097e7
drummermanrob20 <robert@stapleton-family.net>
2022-01-23 14:31:43 -0500

    modified for 3 VM creations

--- DEMO.Azure.VM.Creation.ps1
+++ DEMO.Azure.VM.Creation.ps1
@@ -4,14 +4,17 @@ Connect-AzAccount
 # create resource group
 New-AzResourceGroup -Name "Demo_Resource_Group" -Location 'eastus2'
 
-# create virtual machine
-$splat = @{
-    ResourceGroupName = 'Demo_Resource_Group'
-    Name = 'DEMO-VM-01'
-    Image = 'Win2019Datacenter'
-    Size = 'Standard_B1ms'
-    Credential = Get-Credential
-    OpenPorts = '3389'
+# create virtual machines
+
+for ($i=1; $i -le 3; $i++) {
+    $splat = @{
+        ResourceGroupName = 'Demo_Resource_Group'
+        Name = 'DEMO-VM-0' + $i
+        Image = 'Win2019Datacenter'
+        Size = 'Standard_B1ms'
+        Credential = Get-Credential
+        OpenPorts = '3389'
+    }
 }
 
 New-AzVM @splat
