2e484ee3357b904ad728fd1e980c97c50c8b19b0
drummermanrob20 <robert@stapleton-family.net>
2022-02-05 10:09:07 -0500

    removed unnecessary code

--- DEMO.VM.PowerOff.ps1
+++ DEMO.VM.PowerOff.ps1
@@ -6,12 +6,3 @@ foreach ($vm in $AllVM){
     $VMName = $vm.Name
     Stop-AzVM -Name $VMName -ResourceGroupName 'demo' -Force
 }
-
-$VM = Get-AzVM -Name $VMName -Status
-$PowerState = $VM.PowerState
-$Name = $VM.Name
-
-if ($PowerState -eq "VM running") {
-    Write-Host "About to shut down virtual machine: $Name"
-    Stop-AzVM -Name $Name -ResourceGroupName 'Sandbox'
-}
