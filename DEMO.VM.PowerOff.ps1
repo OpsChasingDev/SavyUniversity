ae025763aafe011544daface62febac45e682627
drummermanrob20 <robert@stapleton-family.net>
2022-02-05 10:36:39 -0500

    typo fixed

--- DEMO.VM.PowerOff.ps1
+++ DEMO.VM.PowerOff.ps1
@@ -1,15 +1,14 @@
 # this script assumes you are already connected to your Azure account
 
-$AllVM = Get-AzResourceGroup 'demo' | Get-AzVM
-
 $Read = Read-Host "Type 'yes' to power off all of the VMs in the DEMO resource group"
 
 if ($Read -eq 'yes'){
+    $AllVM = Get-AzResourceGroup 'demo' | Get-AzVM
     foreach ($vm in $AllVM){
         $VMName = $vm.Name
         Stop-AzVM -Name $VMName -ResourceGroupName 'demo' -Force -AsJob
     }
 }
 else {
-    Write-Output "Well, run me again if you make up your mind.1"
+    Write-Output "Well, run me again if you make up your mind."
 }
