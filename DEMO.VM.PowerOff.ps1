0e692333ad4cf53a4870988edc95e06e8b039eb4
drummermanrob20 <robert@stapleton-family.net>
2022-02-05 10:35:47 -0500

    read-host with if statement to confirm action

--- DEMO.VM.PowerOff.ps1
+++ DEMO.VM.PowerOff.ps1
@@ -2,7 +2,14 @@
 
 $AllVM = Get-AzResourceGroup 'demo' | Get-AzVM
 
-foreach ($vm in $AllVM){
-    $VMName = $vm.Name
-    Stop-AzVM -Name $VMName -ResourceGroupName 'demo' -Force -AsJob
+$Read = Read-Host "Type 'yes' to power off all of the VMs in the DEMO resource group"
+
+if ($Read -eq 'yes'){
+    foreach ($vm in $AllVM){
+        $VMName = $vm.Name
+        Stop-AzVM -Name $VMName -ResourceGroupName 'demo' -Force -AsJob
+    }
+}
+else {
+    Write-Output "Well, run me again if you make up your mind.1"
 }
