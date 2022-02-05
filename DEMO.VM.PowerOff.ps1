1097743f29d2a91946ad242df91c41f812e95947
drummermanrob20 <robert@stapleton-family.net>
2022-02-05 10:39:27 -0500

    options for power off or on

--- DEMO.VM.PowerOff.ps1
+++ DEMO.VM.PowerOff.ps1
@@ -1,12 +1,19 @@
 # this script assumes you are already connected to your Azure account
 
-$Read = Read-Host "Type 'yes' to power off all of the VMs in the DEMO resource group"
+$Read = Read-Host "Type 'ON' to power on all DEMO VMs or 'OFF' to power off all DEMO VMs."
 
-if ($Read -eq 'yes'){
+if ($Read -eq 'OFF'){
     $AllVM = Get-AzResourceGroup 'demo' | Get-AzVM
     foreach ($vm in $AllVM){
         $VMName = $vm.Name
         Stop-AzVM -Name $VMName -ResourceGroupName 'demo' -Force -AsJob
+    }
+}
+elseif ($Read -eq 'ON'){
+    $AllVM = Get-AzResourceGroup 'demo' | Get-AzVM
+    foreach ($vm in $AllVM){
+        $VMName = $vm.Name
+        Start-AzVM -Name $VMName -ResourceGroupName 'demo' -Force -AsJob
     }
 }
 else {
