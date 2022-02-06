4b94d0bd62e7bac1a097140871360138712d28c3
drummermanrob20 <robert@stapleton-family.net>
2022-02-06 09:09:07 -0500

    added force

--- DEMO.VM.Cleanup.ps1
+++ DEMO.VM.Cleanup.ps1
@@ -3,5 +3,5 @@
 $Read = Read-Host "Do you want to remove all non-vnet resources in the Resource Group 'DEMO'?  Type 'yes' to confirm."
 
 if($Read -eq "yes") {
-    Get-AzResource -ResourceGroupName 'demo' | Where-Object {$_.Type -ne "Microsoft.Network/virtualNetworks"} | Remove-AzResource
+    Get-AzResource -ResourceGroupName 'demo' | Where-Object {$_.Type -ne "Microsoft.Network/virtualNetworks"} | Remove-AzResource -Force
 }
