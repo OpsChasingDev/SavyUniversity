d6c68ee3bb049758908281c468781050e60a3a55
drummermanrob20 <robert@stapleton-family.net>
2022-02-06 08:58:25 -0500

    multi-VM support - running as job

--- DEMO.Infra.Build.ps1
+++ DEMO.Infra.Build.ps1
@@ -1,6 +1,11 @@
 # uses a series of constructions to generate attributes of a virtual machine, its virtual network, and its credentials
 # the attributes are stored together in a VMConfig and then is passed to the VM creation at the end
 
+param (
+    [Parameter(Mandatory)]
+    [int]$VMNumber
+)
+
 # identify resource group and region
 $ResourceGroupName = "DEMO"
 $Location = "eastus2"
@@ -22,29 +27,31 @@ $VirtualNetwork = New-AzVirtualNetwork @VirutalNetworkSplat
 $SubnetId = $VirtualNetwork.Subnets.Id
 
 # construct network adapter
-$NICName = "DEMO-VM-1_NIC"
-$NICSplat = @{
-    Name = $NICName
-    ResourceGroupName = $ResourceGroupName
-    Location = $Location
-    SubnetId = $SubnetId
+for ($v = 1; $v -le $VMNumber; $v++) {
+    $NICName = "DEMO-VM-" + $v + "_NIC"
+    $NICSplat = @{
+        Name = $NICName
+        ResourceGroupName = $ResourceGroupName
+        Location = $Location
+        SubnetId = $SubnetId
+    }
+    $NIC = New-AzNetworkInterface @NICSplat
+
+    # construct VM local credentials
+    $LocalAdmin = "RStapleton"
+    $LocalPass = ConvertTo-SecureString "SomethingB3tt3r!@#" -AsPlainText -Force
+    $Credential = New-Object -TypeName System.Management.Automation.PSCredential ($LocalAdmin,$LocalPass)
+
+    # construct VM attributes
+    $Name = "DEMO-VM-" + $v
+    $Size = "Standard_B1ms"
+
+    # construct VM object
+    $VM = New-AzVMConfig -VMName $Name -VMSize $Size
+    $VM = Set-AzVMOperatingSystem -VM $VM -Windows -ComputerName $Name -Credential $Credential -ProvisionVMAgent -EnableAutoUpdate
+    $VM = Add-AzVMNetworkInterface -VM $VM -Id $NIC.Id
+    $VM = Set-AzVMSourceImage -VM $VM -PublisherName 'MicrosoftWindowsServer' -Offer 'WindowsServer' -Skus '2019-Datacenter-Core' -Version latest
+
+    # make VM
+    New-AzVM -VM $VM -ResourceGroupName $ResourceGroupName -Location $Location -AsJob
 }
-$NIC = New-AzNetworkInterface @NICSplat
-
-# construct VM local credentials
-$LocalAdmin = "RStapleton"
-$LocalPass = ConvertTo-SecureString "SomethingB3tt3r!@#" -AsPlainText -Force
-$Credential = New-Object -TypeName System.Management.Automation.PSCredential ($LocalAdmin,$LocalPass)
-
-# construct VM attributes
-$Name = "DEMO-VM-1"
-$Size = "Standard_B1ms"
-
-# construct VM object
-$VM = New-AzVMConfig -VMName $Name -VMSize $Size
-$VM = Set-AzVMOperatingSystem -VM $VM -Windows -ComputerName $Name -Credential $Credential -ProvisionVMAgent -EnableAutoUpdate
-$VM = Add-AzVMNetworkInterface -VM $VM -Id $NIC.Id
-$VM = Set-AzVMSourceImage -VM $VM -PublisherName 'MicrosoftWindowsServer' -Offer 'WindowsServer' -Skus '2019-Datacenter-Core' -Version latest
-
-# make VM
-New-AzVM -VM $VM -ResourceGroupName $ResourceGroupName -Location $Location -Verbose
