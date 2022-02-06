cc57d3bc3890d6889518e4a440a57b4de9b7ac90
drummermanrob20 <robert@stapleton-family.net>
2022-02-06 09:06:13 -0500

    made conditions specific to resource type

--- DEMO.VM.Cleanup.ps1
+++ DEMO.VM.Cleanup.ps1
@@ -1,2 +1,2 @@
 # removes all resources from the DEMO resourcegroup except the VNET
-Get-AzResource -ResourceGroupName 'demo' | Where-Object name -like "*demo-vm-*" | Remove-AzResource -WhatIf
+Get-AzResource -ResourceGroupName 'demo' | Where-Object {$_.Type -ne "Microsoft.Network/virtualNetworks"} | Remove-AzResource
