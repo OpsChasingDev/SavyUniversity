95df28ca96cc1bab659c92cf757412e720c2135c
drummermanrob20 <robert@stapleton-family.net>
2022-02-06 15:54:45 -0500

    VM removal in parallel

--- DEMO.VM.Cleanup.ps1
+++ DEMO.VM.Cleanup.ps1
@@ -3,9 +3,39 @@
 
 $Read = Read-Host "Do you want to remove all non-vnet resources in the Resource Group 'DEMO'?  Type 'yes' to confirm."
 
+<#
 if($Read -eq "yes") {
     do {
         $Resource = Get-AzResource -ResourceGroupName 'demo' | Where-Object {$_.Type -ne "Microsoft.Network/virtualNetworks"}
         $Resource | Remove-AzResource -Force -ErrorAction SilentlyContinue
     } while ($Resource)
 }
+#>
+
+# break
+
+if ($Read -eq "yes"){
+    $VirtualMachines = Get-AzResource -ResourceGroupName "demo" | Where-Object {$_.Type -eq "Microsoft.Compute/virtualMachines"}
+    foreach ($m in $VirtualMachines) {
+        $m | Remove-AzResource -Force -AsJob
+    }
+    do {
+        $Job = Get-Job | Where-Object {$_.State -eq 'Running'}
+        Write-Verbose "Working..."
+        Start-Sleep -Seconds 5
+    } while ($Job)
+    Write-Verbose "All virtual machines removed."
+}
+
+
+<#
+Microsoft.Network/networkInterfaces
+Microsoft.Compute/virtualMachines
+Microsoft.Compute/disks
+
+Get-Job | where {$_.State -eq 'running'}
+
+Members:
+Id
+State (Running, Completed)
+#>
