3c4b1ab961092ae957fe2b9b83d088cedfa4e60f
drummermanrob20 <robert@stapleton-family.net>
2022-02-10 20:25:45 -0500

    process as backup jobs

--- DEMO.Infra.Build.Peer.ps1
+++ DEMO.Infra.Build.Peer.ps1
@@ -1,5 +1,5 @@
 $VNET_Demo = Get-AzVirtualNetwork -ResourceGroupName 'demo'
 $VNET_Sandbox = Get-AzVirtualNetwork -ResourceGroupName 'sandbox'
 
-Add-AzVirtualNetworkPeering -Name "DEMO_to_Sandbox" -VirtualNetwork $VNET_Demo -RemoteVirtualNetworkId $VNET_Sandbox.Id
-Add-AzVirtualNetworkPeering -Name "Sandbox_to_DEMO" -VirtualNetwork $VNET_Sandbox -RemoteVirtualNetworkId $VNET_Demo.Id
+$VNET_Demo_Job = Add-AzVirtualNetworkPeering -Name "DEMO_to_Sandbox" -VirtualNetwork $VNET_Demo -RemoteVirtualNetworkId $VNET_Sandbox.Id -AsJob
+$VNET_Sandbox_Job = Add-AzVirtualNetworkPeering -Name "Sandbox_to_DEMO" -VirtualNetwork $VNET_Sandbox -RemoteVirtualNetworkId $VNET_Demo.Id -AsJob
