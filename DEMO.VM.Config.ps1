1a71c70c6b7115b0ebc3d6ff399cee4e72a083e8
drummermanrob20 <robert@stapleton-family.net>
2022-02-27 10:29:22 -0500

    added get credential

--- DEMO.VM.Config.ps1
+++ DEMO.VM.Config.ps1
@@ -2,6 +2,8 @@
 Script to run against all remote VMs once powershell remoting is enabled in order to configure the services hosted on the VMs
 #>
 
+$cred = Get-Credential
+
 $VMCount = (Get-AzVM -ResourceGroupName "DEMO").Count
 $Collection = @()
 
@@ -10,10 +12,7 @@ for ($i = 0; $i -lt $VMCount; $i++) {
     $Collection += "192.168.0.$Octet"
 }
 
-Write-Output $Collection
-
 # install web services
-
 Invoke-Command $Collection -Credential $cred {
-    Install-WindowsFeature web-server -Restart -IncludeManagementTools
+    Install-WindowsFeature 'Web-Server' -Restart -IncludeManagementTools
 }
