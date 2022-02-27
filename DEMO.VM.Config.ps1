db0432301b61691f07ac3ea155a7e5239f73b2e3
drummermanrob20 <robert@stapleton-family.net>
2022-02-27 09:54:29 -0500

    counting VMs in resourcegroup

--- DEMO.VM.Config.ps1
+++ DEMO.VM.Config.ps1
@@ -1,5 +1,11 @@
+<#
+Script to run against all remote VMs once powershell remoting is enabled in order to configure the services hosted on the VMs
+#>
+
+$VMCount = (Get-AzVM -ResourceGroupName "DEMO").Count
 
 
+# install web services
 Invoke-Command $demo -Credential $cred {
     Install-WindowsFeature web-server -Restart -IncludeManagementTools
 }
