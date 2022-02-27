d9d1db65570ef61d07bbaab07639ecc47fcd8910
drummermanrob20 <robert@stapleton-family.net>
2022-02-27 10:11:53 -0500

    for loop to create IP address list based on VMs

--- DEMO.VM.Config.ps1
+++ DEMO.VM.Config.ps1
@@ -3,9 +3,17 @@ Script to run against all remote VMs once powershell remoting is enabled in orde
 #>
 
 $VMCount = (Get-AzVM -ResourceGroupName "DEMO").Count
+$Collection = @()
 
+for ($i = 0; $i -lt $VMCount; $i++) {
+    $Octet = $i + 4
+    $Collection += "192.168.0.$Octet"
+}
+
+Write-Output $Collection
 
 # install web services
-Invoke-Command $demo -Credential $cred {
+
+Invoke-Command $Collection -Credential $cred {
     Install-WindowsFeature web-server -Restart -IncludeManagementTools
 }
