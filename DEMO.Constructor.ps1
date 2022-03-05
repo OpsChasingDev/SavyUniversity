cb0099f8a3bb0737fe137d8ae9c3714b31d1fecc
drummermanrob20 <robert@stapleton-family.net>
2022-03-05 10:01:53 -0500

    constructor orer

--- DEMO.Constructor.ps1
+++ DEMO.Constructor.ps1
@@ -1,1 +1,14 @@
-Constructor.ps1
+# The targeted final goal for a "push button" method of full infrastructure deployment
+# Comments below list the order of operations under which the infrastructure will need to be built
+
+# resource group
+
+# virtual network
+
+# peering
+
+# availability set
+
+# VMs, including disks, NICs, and assigning to the availability set
+
+# public IP and load balancer
