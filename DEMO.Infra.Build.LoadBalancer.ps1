f28b4787c05eb34696e379ce84c46d4faa3a94df
drummermanrob20 <robert@stapleton-family.net>
2022-03-08 20:43:43 -0500

    combined public IP with LB code

--- DEMO.Infra.Build.LoadBalancer.ps1
+++ DEMO.Infra.Build.LoadBalancer.ps1
@@ -4,16 +4,17 @@ $Location = "eastus2"
 $PublicIPName = 'DEMO_PublicIP'
 $AllocationMethod = "dynamic"
 
-# define values for load balancer
-$LBName = "DEMO_LoadBalancer"
-
+# create public IP
 $PublicIPSplat = @{
     Name = $PublicIPName
     ResourceGroupName = $ResourceGroupName
     Location = $Location
     AllocationMethod = $AllocationMethod
 }
+$PublicIP = New-AzPublicIpAddress @PublicIPSplat
 
-New-AzPublicIpAddress @PublicIPSplat
+# define values for load balancer
+$LBName = "DEMO_LoadBalancer"
+$LBFrontEnd = New-AzLoadBalancerFrontendIpConfig -Name "DEMO_LBFrontEnd" -PublicIpAddress $PublicIP
 
 New-AzLoadBalancer -
