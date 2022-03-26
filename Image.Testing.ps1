19175c0b5ba3d45cca94339d57f6ff8c4cc6d5f8
drummermanrob20 <robert@stapleton-family.net>
2022-03-26 09:04:50 -0400

    experimenting

--- Image.Testing.ps1
+++ Image.Testing.ps1
@@ -1,13 +1,26 @@
+# https://www.youtube.com/watch?v=_b5T-dPpd00
+
 $Name = 'VM'
 $ResourceGroupName = 'DEMO'
 $Location = 'eastus2'
 $ImageName = 'Server-2022-Core-WinRM'
-$VHDUri = 'https://savylabsandbox.blob.core.windows.net/vhdx/Server-2022-Core-WinRM.vhd'
+$ImageURI = 'https://savylabsandbox.blob.core.windows.net/vhdx/Server-2022-Core-WinRM.vhd'
+$VHDUri = 'https://savylabsandbox.blob.core.windows.net/disks'
+$LocalAdmin = "RStapleton"
+$LocalPass = ConvertTo-SecureString "SomethingB3tt3r!@#" -AsPlainText -Force
+$Credential = New-Object -TypeName System.Management.Automation.PSCredential ($LocalAdmin,$LocalPass)
+$VirtualNetwork = Get-AzVirtualNetwork -Name 'DEMO_VNET'
+$SubnetId = $VirtualNetwork.Subnets.Id
+$NICSplat = @{
+    Name = 'testvm_NIC'
+    ResourceGroupName = $ResourceGroupName
+    Location = $Location
+    SubnetId = $SubnetId
+}
+$NIC = New-AzNetworkInterface @NICSplat
 
-$ImageConfig = New-AzImageConfig -Location $Location
-$ImageConfig = Set-AzImageOsDisk -Image $ImageConfig -OsType Windows -OsState generalized -BlobUri $VHDUri
-$Image = New-AzImage -ImageName $ImageName -ResourceGroupName $ResourceGroupName -Image $ImageConfig
-
-Add-AzVhd -ResourceGroupName $ResourceGroupName -Destination $VHDUri -LocalFilePath 'D:\Hyper-V\Server-2022-Core-WinRM_Converted.vhd' -OverWrite
-
-#New-AzVM -ResourceGroupName $ResourceGroupName -Name $Name -
+$VM = New-AzVMConfig -VMName 'testvm' -VMSize "Standard_B1ms"
+$VM = Add-AzVMNetworkInterface -VM $VM -Id $NIC.Id
+$VM = Set-AzVMOperatingSystem -VM $VM -Windows -ComputerName "TestVM" -Credential $Credential
+$VM = Set-AzVMOSDisk -VM $VM -Name "osDisk.vhd" -SourceImageUri $ImageURI -CreateOption fromImage -Windows
+New-AzVM -VM $VM -ResourceGroupName $ResourceGroupName -Location $Location
