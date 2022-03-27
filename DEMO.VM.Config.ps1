5bbe47649f0b493858ba6ce76e5e0284739cf395
drummermanrob20 <robert@stapleton-family.net>
2022-03-27 10:03:08 -0400

    confirmed vm config automation

--- DEMO.VM.Config.ps1
+++ DEMO.VM.Config.ps1
@@ -2,8 +2,9 @@
 Script to run against all remote VMs once powershell remoting is enabled in order to configure the services hosted on the VMs
 #>
 
-$cred = Get-Credential
-Connect-AzAccount
+$LocalAdmin = "RStapleton"
+$LocalPass = ConvertTo-SecureString "SomethingB3tt3r!@#" -AsPlainText -Force
+$Credential = New-Object -TypeName System.Management.Automation.PSCredential ($LocalAdmin,$LocalPass)
 
 $VMCount = (Get-AzVM -ResourceGroupName "DEMO").Count
 $Collection = @()
@@ -14,12 +15,12 @@ for ($i = 0; $i -lt $VMCount; $i++) {
 }
 
 # install web services
-Invoke-Command $Collection -Credential $cred {
+Invoke-Command $Collection -Credential $Credential {
     Install-WindowsFeature 'Web-Server' -Restart -IncludeManagementTools
 }
 
 # copy custom web image
-$Session = New-PSSession -ComputerName $Collection -Credential $cred
+$Session = New-PSSession -ComputerName $Collection -Credential $Credential
 foreach ($s in $Session) {
     Copy-Item -Path 'C:\Root\hello.png' -ToSession $s -Destination 'C:\inetpub\wwwroot\iisstart.png'
 }
