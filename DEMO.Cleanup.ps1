f2ffbc4719141e5723fb00e811e3512710a8bce4
drummermanrob20 <robert@stapleton-family.net>
2022-03-31 19:57:36 -0400

    dependent parameters fixed

--- DEMO.Cleanup.ps1
+++ DEMO.Cleanup.ps1
@@ -1,9 +1,9 @@
 $Answer = Read-Host "Are you sure you want to remove the Resource Group 'DEMO' and all of its components? [Y/N]"
-
+$ResourceGroupName = 'Sandbox'
 if ($Answer -eq 'Y') {
-   
-    $VNET = Get-AzVirtualNetworkPeering -ResourceGroupName 'Sandbox' -VirtualNetworkName 'Sandbox-vnet'
-    Remove-AzVirtualNetworkPeering -Name $VNET.Name
+    $VNET = Get-AzVirtualNetwork -ResourceGroupName $ResourceGroupName
+    $VNETPeer = Get-AzVirtualNetworkPeering -VirtualNetworkName $VNET.Name -ResourceGroupName $ResourceGroupName
+    Remove-AzVirtualNetworkPeering -Name $VNETPeer -VirtualNetworkName $VNET.name -ResourceGroupName $ResourceGroupName
+}
     
     Remove-AzResourceGroup -Name 'DEMO'
-}
