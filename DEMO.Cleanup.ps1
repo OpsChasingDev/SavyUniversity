f11ca34b1aaa7e73a09e2adc9ad07a58b8f0f948
drummermanrob20 <robert@stapleton-family.net>
2022-03-31 20:05:09 -0400

    added verbose

--- DEMO.Cleanup.ps1
+++ DEMO.Cleanup.ps1
@@ -1,9 +1,15 @@
-$Answer = Read-Host "Are you sure you want to remove the Resource Group 'DEMO' and all of its components? [Y/N]"
+$Answer = Read-Host "Are you sure you want to remove the resource group 'DEMO' and all of its components? [Y/N]"
 $ResourceGroupName = 'Sandbox'
 if ($Answer -eq 'Y') {
+    
+    Write-Verbose "Removing network peer from the remote side in $ResourceGroupName..."
     $VNET = Get-AzVirtualNetwork -ResourceGroupName $ResourceGroupName
     $VNETPeer = Get-AzVirtualNetworkPeering -VirtualNetworkName $VNET.Name -ResourceGroupName $ResourceGroupName
-    Remove-AzVirtualNetworkPeering -Name $VNETPeer.Name -VirtualNetworkName $VNET.Name -ResourceGroupName $ResourceGroupName
+    Remove-AzVirtualNetworkPeering -Name $VNETPeer.Name -VirtualNetworkName $VNET.Name -ResourceGroupName $ResourceGroupName -Force
+    Write-Verbose "Removing network peer completed."
+
+    Write-Verbose "Removing resource group 'DEMO'.  This may take a few minutes..."
+    Remove-AzResourceGroup -Name 'DEMO' -Force
+    Write-Verbose "Removing resource group 'DEMO' completed."
 }
     
-Remove-AzResourceGroup -Name 'DEMO'
