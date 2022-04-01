90ffbfdcdfbf6da442a54757d28580bab2b606c7
drummermanrob20 <robert@stapleton-family.net>
2022-03-31 20:14:00 -0400

    fix: wrong param name

--- DEMO.Cleanup.ps1
+++ DEMO.Cleanup.ps1
@@ -4,9 +4,9 @@ $Answer = Read-Host "Are you sure you want to remove the resource group $Resourc
 if ($Answer -eq 'Y') {
     
     Write-Verbose "Removing network peer from the remote side in $RemoteResourceGroup..."
-    $VNET = Get-AzVirtualNetwork -RemoteResourceGroup $RemoteResourceGroup
-    $VNETPeer = Get-AzVirtualNetworkPeering -VirtualNetworkName $VNET.Name -RemoteResourceGroup $RemoteResourceGroup
-    Remove-AzVirtualNetworkPeering -Name $VNETPeer.Name -VirtualNetworkName $VNET.Name -RemoteResourceGroup $RemoteResourceGroup -Force
+    $VNET = Get-AzVirtualNetwork -ResourceGroupName $RemoteResourceGroup
+    $VNETPeer = Get-AzVirtualNetworkPeering -VirtualNetworkName $VNET.Name -ResourceGroupName $RemoteResourceGroup
+    Remove-AzVirtualNetworkPeering -Name $VNETPeer.Name -VirtualNetworkName $VNET.Name -ResourceGroupName $RemoteResourceGroup -Force
     Write-Verbose "Network peer removed from $RemoteResourceGroup."
 
     Write-Verbose "Removing resource group $ResourceGroupName.  This may take a few minutes..."
