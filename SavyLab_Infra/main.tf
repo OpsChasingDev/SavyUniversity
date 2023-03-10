0004ccf9c69f61822e0ceec3c4ef139c201f2403
drummermanrob20 <robert@stapleton-family.net>
2023-03-10 10:52:23 -0500

    using tfvars file for params

--- SavyLab_Infra/main.tf
+++ SavyLab_Infra/main.tf
@@ -1,43 +1,49 @@
-provider "azurerm" {
-  features {}
+variable "location" {
+  type    = string
+  default = "East US"
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = "Savy_University"
-  location = "East US"
+variable "resource_group_name" {
+  type    = string
+  default = "Savy_University"
+}
+
+provider "azurerm" {
+  features {}
 }
 
 resource "azurerm_virtual_network" "vnet" {
   name                = "sl-vnet"
   address_space       = ["10.250.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
 }
 
 resource "azurerm_subnet" "subnet" {
   name                 = "sl-subnet"
-  resource_group_name  = azurerm_resource_group.rg.name
+  resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.250.1.0/24"]
 }
 
 resource "azurerm_public_ip" "public_ip" {
   name                = "sl-public-ip"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
   allocation_method   = "Static"
 }
 
 resource "azurerm_nat_gateway" "nat_gateway" {
-  name                 = "sl-nat-gateway"
-  location             = azurerm_resource_group.rg.location
-  resource_group_name  = azurerm_resource_group.rg.name
+  name                = "sl-nat-gateway"
+  location            = var.location
+  resource_group_name = var.resource_group_name
   public_ip_address_id = azurerm_public_ip.public_ip.id
-  subnet_id            = azurerm_subnet.subnet.id
-  depends_on           = [azurerm_subnet.subnet]
+  subnet_id           = azurerm_subnet.subnet.id
+  depends_on          = [azurerm_subnet.subnet]
 }
 
 resource "azurerm_subnet_network_security_group_association" "subnet_nsg_association" {
   subnet_id                 = azurerm_subnet.subnet.id
   network_security_group_id = azurerm_nat_gateway.nat_gateway.network_security_group_id
 }
+
