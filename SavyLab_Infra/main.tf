513b309e0d26f09e84307e03e899eb1a624a9caf
drummermanrob20 <robert@stapleton-family.net>
2023-03-10 15:35:58 -0500

    standard sku for public ip

--- SavyLab_Infra/main.tf
+++ SavyLab_Infra/main.tf
@@ -33,6 +33,7 @@ resource "azurerm_public_ip" "public_ip" {
   location            = var.location
   resource_group_name = var.resource_group_name
   allocation_method   = "Static"
+  sku                 = "Standard"
 }
 
 #FIXME: unexpected arguments "public_ip_address_id" and "subnet_id"
