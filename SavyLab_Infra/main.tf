4783f5b5c5c9b00c2333a4825d6df1011e7d043a
drummermanrob20 <robert@stapleton-family.net>
2023-03-10 10:42:48 -0500

    changed resource name prefix from "my-" to "sl-"

--- SavyLab_Infra/main.tf
+++ SavyLab_Infra/main.tf
@@ -3,33 +3,33 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "my-resource-group"
+  name     = "Savy_University"
   location = "East US"
 }
 
 resource "azurerm_virtual_network" "vnet" {
-  name                = "my-vnet"
+  name                = "sl-vnet"
   address_space       = ["10.250.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
 
 resource "azurerm_subnet" "subnet" {
-  name                 = "my-subnet"
+  name                 = "sl-subnet"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.250.1.0/24"]
 }
 
 resource "azurerm_public_ip" "public_ip" {
-  name                = "my-public-ip"
+  name                = "sl-public-ip"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Static"
 }
 
 resource "azurerm_nat_gateway" "nat_gateway" {
-  name                 = "my-nat-gateway"
+  name                 = "sl-nat-gateway"
   location             = azurerm_resource_group.rg.location
   resource_group_name  = azurerm_resource_group.rg.name
   public_ip_address_id = azurerm_public_ip.public_ip.id
