04770623747e5706dac1a6dcfd64f07c92db1df6
drummermanrob20 <robert@stapleton-family.net>
2023-03-13 09:06:46 -0400

    removed public ip allocation from VM

--- SavyLab_Infra/main.tf
+++ SavyLab_Infra/main.tf
@@ -87,7 +87,6 @@ resource "azurerm_network_interface" "nic" {
     name                          = "nicconfig"
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.public_ip.id
   }
 }
 
