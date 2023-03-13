d90a186f611d9cd78d0163c5b27a74ff88bdd7e0
drummermanrob20 <robert@stapleton-family.net>
2023-03-13 09:32:10 -0400

    adjust syntax for public IP on NIC

--- SavyLab_Infra/main.tf
+++ SavyLab_Infra/main.tf
@@ -95,7 +95,7 @@ resource "azurerm_network_interface" "nic" {
     name                          = "nicconfig"
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_allocation  = azurerm_public_ip.public_ip_server.id
+    public_ip_address_id  = azurerm_public_ip.public_ip_server.id
   }
 }
 
