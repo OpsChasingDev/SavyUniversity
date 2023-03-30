d3c91a8c40a78c286417a2e100b5c34e1039c919
OpsChasingDev <robert@stapleton-family.net>
2023-03-30 14:50:42 -0400

    removed old VM config

--- SavyLab_Infra/main.tf
+++ SavyLab_Infra/main.tf
@@ -116,29 +116,6 @@ resource "azurerm_network_interface" "nic" {
   }
 }
 
-# resource "azurerm_windows_virtual_machine" "vm" {
-#   name                  = "sl-vm"
-#   location              = var.location
-#   resource_group_name   = var.resource_group_name
-#   network_interface_ids = [azurerm_network_interface.nic.id]
-#   size                  = "Standard_F2"
-#   admin_username        = var.vm_username
-#   admin_password        = var.vm_password
-
-#   source_image_reference {
-#     publisher = "MicrosoftWindowsServer"
-#     offer     = "WindowsServer"
-#     sku       = "2019-Datacenter"
-#     version   = "latest"
-#   }
-
-#   os_disk {
-#     name                 = "myOsDisk"
-#     caching              = "ReadWrite"
-#     storage_account_type = "Standard_LRS"
-#   }
-# }
-
 resource "azurerm_image" "image" {
   name                = "sl-image"
   location            = var.location
